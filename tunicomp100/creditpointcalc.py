d7c38132b2312542a0d23e0d60f52207c7e1c9d4
Ishan Malu <ishan.malu@gmail.com>
2025-09-12 14:52:54 +0300

    update

--- tunicomp100/creditpointcalc.py
+++ tunicomp100/creditpointcalc.py
@@ -4,33 +4,27 @@ Creator: Ishan Malu
 Student id number: 154138420
 """
 
-
 def main():
     months = int(input("Enter the number of months: "))
     total_credits = 0
-    consecutive_zero = 0
+    previous_zero = False
 
     for i in range(1, months + 1):
         credits = int(input(f"Enter the number of credits in month {i}: "))
-        total_credits += credits
         if credits == 0:
-            consecutive_zero += 1
-            if consecutive_zero == 2:
-                print("You did have too many study breaks!")
-                print()
+            if previous_zero:
+                print("You did have too many study breaks!", end="")
                 return
+            previous_zero = True
         else:
-            consecutive_zero = 0
+            previous_zero = False
+        total_credits += credits
 
     average = total_credits / months
     if average >= 5:
-        print(
-            f"You are a full time student and your monthly credit point average is {average:.1f}.")
+        print(f"You are a full time student and your monthly credit point average is {average:.1f}.", end="")
     else:
-        print(
-            f"Your monthly credit point average {average:.1f} does not classify you as a full time student.")
+        print(f"Your monthly credit point average {average:.1f} does not classify you as a full time student.", end="")
 
-    print()
-
-
-main()
+if __name__ == "__main__":
+    main()
---
+++ tunicomp100/creditpointcalculator.py
@@ -0,0 +1,30 @@
+"""
+COMP.CS.100 Credit Point Calculator
+Creator: Ishan Malu
+Student id number: 154138420
+"""
+
+def main():
+    months = int(input("Enter the number of months: "))
+    total_credits = 0
+    previous_zero = False
+
+    for i in range(1, months + 1):
+        credits = int(input(f"Enter the number of credits in month {i}: "))
+        if credits == 0:
+            if previous_zero:
+                print("\nYou did have too many study breaks!", end="")
+                return
+            previous_zero = True
+        else:
+            previous_zero = False
+        total_credits += credits
+
+    average = total_credits / months
+    if average >= 5:
+        print(f"\nYou are a full time student and your monthly credit point average is {average:.1f}.", end="")
+    else:
+        print(f"\nYour monthly credit point average {average:.1f} does not classify you as a full time student.", end="")
+
+if __name__ == "__main__":
+    main()
